435a0bf6363d6fed7561f8e63ebe278fa0ef5e37
devfle <hello@flesch-design.de>
2022-07-07 15:51:15 +0200

    feat(blog-page): render json data into blog page - added basic styles for blog page - if you want to add blog data, use blog-data.json - TODO: Fix Script / JSON module import in Safari

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,1 +1,32 @@
 import blogData from "./../json/blog-data.json" assert { type: "json" };
+
+const blogWrapper = document.querySelector(".content");
+
+const createBlogEntry = (blogEntry) => {
+  const blogEntryWrapper = document.createElement("div");
+  const blogEntryTitle = document.createElement("h1");
+  const blogEntryTitleTextNode = document.createTextNode(blogEntry.title);
+
+  const blogEntryContent = document.createElement("p");
+  const blogEntryContentTextNode = document.createTextNode(blogEntry.content);
+
+  const blogEntryInfo = document.createElement("span");
+  const blogEntryInfoTextNode = document.createTextNode(`From: ${blogEntry.author} / Published: ${blogEntry.date}`);
+
+  blogEntryTitle.appendChild(blogEntryTitleTextNode);
+  blogEntryWrapper.appendChild(blogEntryTitle);
+
+  blogEntryInfo.appendChild(blogEntryInfoTextNode);
+  blogEntryWrapper.appendChild(blogEntryInfo);
+
+  blogEntryContent.appendChild(blogEntryContentTextNode);
+  blogEntryWrapper.appendChild(blogEntryContent);
+
+  blogEntryWrapper.classList.add("blog-entry");
+  return blogEntryWrapper;
+};
+
+for (const blogEntry of blogData) {
+  const blogDom = createBlogEntry(blogEntry);
+  blogWrapper.appendChild(blogDom);
+}
