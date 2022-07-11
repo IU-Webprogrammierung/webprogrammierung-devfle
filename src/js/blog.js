abda58a875eead744122908de7a836e86b9d6b70
devfle <hello@flesch-design.de>
2022-07-11 23:46:01 +0200

    fix(blog-page): import json with fetch api to increase browser support

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,16 +1,12 @@
-import blogData from "./../json/blog-data.json" assert { type: "json" };
-
-const blogWrapper = document.querySelector(".content");
-
 const createBlogEntry = (blogEntry) => {
-  const blogEntryWrapper = document.createElement("article");
-  const blogEntryTitle = document.createElement("h1");
+  const blogEntryWrapper = document.createElement('article');
+  const blogEntryTitle = document.createElement('h1');
   const blogEntryTitleTextNode = document.createTextNode(blogEntry.title);
 
-  const blogEntryContent = document.createElement("p");
+  const blogEntryContent = document.createElement('p');
   const blogEntryContentTextNode = document.createTextNode(blogEntry.content);
 
-  const blogEntryInfo = document.createElement("span");
+  const blogEntryInfo = document.createElement('span');
   const blogEntryInfoTextNode = document.createTextNode(`From: ${blogEntry.author} / Published: ${blogEntry.date}`);
 
   blogEntryTitle.appendChild(blogEntryTitleTextNode);
@@ -22,14 +18,20 @@ const createBlogEntry = (blogEntry) => {
   blogEntryContent.appendChild(blogEntryContentTextNode);
   blogEntryWrapper.appendChild(blogEntryContent);
 
-  blogEntryInfo.classList.add("blog-entry__info");
-  blogEntryTitle.classList.add("blog-entry__title");
-  blogEntryContent.classList.add("blog-entry__content");
-  blogEntryWrapper.classList.add("blog-entry");
+  blogEntryInfo.classList.add('blog-entry__info');
+  blogEntryTitle.classList.add('blog-entry__title');
+  blogEntryContent.classList.add('blog-entry__content');
+  blogEntryWrapper.classList.add('blog-entry');
   return blogEntryWrapper;
 };
 
-for (const blogEntry of blogData) {
-  const blogDom = createBlogEntry(blogEntry);
-  blogWrapper.appendChild(blogDom);
-}
+const blogWrapper = document.querySelector('.content');
+(async () => {
+  const response = await fetch('./../json/blog-data.json');
+  const blogData = await response.json();
+
+  blogData.forEach((blogEntry) => {
+    const blogDom = createBlogEntry(blogEntry);
+    blogWrapper.appendChild(blogDom);
+  });
+})();
