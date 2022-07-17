e1819fa7a631c191901e0c1c728c7efd12ed8d7b
devfle <hello@flesch-design.de>
2022-07-17 14:27:49 +0200

    feat(darkmode): add darkmode to single sites - TODO: we have to persist the darkmode state - TODO: we have to check user preferrences in media query for darkmode

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,14 @@
-const burgerBtn = document.querySelector(".header__burger-btn");
-const navigation = document.querySelector(".navigation");
+const burgerBtn = document.querySelector('.header__burger-btn');
+const navigation = document.querySelector('.navigation');
 
 // toggles class on click on burger menu to open mobile navigation
 if (!!burgerBtn && !!navigation) {
-  burgerBtn.addEventListener("click", () => {
-    navigation.classList.toggle("navigation--open");
+  burgerBtn.addEventListener('click', () => {
+    navigation.classList.toggle('navigation--open');
   });
 }
+
+const darkModeButton = document.querySelector('.header__icon--darkmode');
+darkModeButton.addEventListener('click', () => {
+  document.documentElement.classList.toggle('darkmode--on');
+});
