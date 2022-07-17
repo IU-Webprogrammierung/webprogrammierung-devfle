770a119fbcd1968b267a20eb81689b06f96334aa
devfle <hello@flesch-design.de>
2022-07-17 17:35:56 +0200

    feat(darkmode): save darkmode settings on site switch - TODO: improve js code

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,25 @@ if (!!burgerBtn && !!navigation) {
   });
 }
 
+let darkModeSettings = JSON.parse(window.localStorage.getItem('darkModeState'));
+
+if (darkModeSettings) {
+  document.documentElement.classList.add('darkmode--on');
+}
+
+// null is been returned if darkmode toggle is been never used
+if (darkModeSettings === null) {
+  darkModeSettings = false;
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.classList.add('darkmode--on');
+    darkModeSettings = true;
+  }
+}
+
 const darkModeButton = document.querySelector('.header__icon--darkmode');
 darkModeButton.addEventListener('click', () => {
+  console.log(darkModeSettings);
+  darkModeSettings = !darkModeSettings;
+  window.localStorage.setItem('darkModeState', darkModeSettings);
   document.documentElement.classList.toggle('darkmode--on');
 });
